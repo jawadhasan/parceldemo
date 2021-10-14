8c2fff543a2b5a4f501d1b0af2c3368ae08216f0
Jawad Shani <jawad.shani@hitachi-eu.com>
2021-10-14 15:02:07 +0200

    jquery jquery-ui imports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 // import another component
 import main from './main';
 
+//jquery and jquery-ui
+import "./import-jquery.js";
+import "jquery-ui-dist/jquery-ui.js";
+
+
+
 main();
+
+$(function() {
+    $("#datepicker").datepicker();
+  });
