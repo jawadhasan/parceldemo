b8baefd1194076c5ee7b432868b8a8620b59b1e5
Jawad Shani <jawad.shani@hitachi-eu.com>
2021-10-15 12:38:47 +0200

    bootstrap fontawesome

--- src/index.js
+++ src/index.js
@@ -5,10 +5,24 @@ import main from './main';
 import "./import-jquery.js";
 import "jquery-ui-dist/jquery-ui.js";
 
+//bootstrap and fontawsome
+import "bootstrap/dist/css/bootstrap.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+
+
+// Import all plugins
+import * as bootstrap from 'bootstrap';
+
+// Or import only needed plugins
+//import { Tooltip as Tooltip, Toast as Toast, Popover as Popover } from 'bootstrap';
+
+
 
 
 main();
 
 $(function() {
     $("#datepicker").datepicker();
+
+    $('[data-toggle="popover"]').popover();
   });
